703cd304968709f5c14c612310fd6b8dfc9a0637
Anwaar Ali <ali.anwaar73@gmail.com>
2020-12-26 19:45:51 +0000

    compile.js script initiated for Campaign.sol

---
+++ kickstart/ethereum/compile.js
@@ -0,0 +1,33 @@
+// This compile script is going to be a bit different from the
+// lottery contract's compile script in that we will only need to
+// run it once and then stored the compiled output (in json form for the interface or ABI) to the build directory for future use
+// !Contents from the build directory can then used to port our contract to other platforms such as truffle suite!
+
+const path = require('path');
+const solc = require('solc');
+const fs = require('fs-extra');
+
+// first we remove the build directory if it already exists
+// __dirname is the reference to the current directory
+// then we traverse the path to our desired location with , seperated "" list
+const buildPath = path.resolve(__dirname, 'build');
+fs.removeSync(buildPath);
+
+// Path to our smart contract file
+const campaignPath = path.resolve(__dirname, 'contracts', 'Campaign.sol');
+
+// Now we read in the source code of from our contract file with the contract path and text encoding arguments
+const source = fs.readFileSync(campaignPath, 'utf8');
+
+// Now the actual compilation step
+// Output will contain our CampaignFactory and Campaign contracts
+const output = solc.compile(source, 1).contracts;
+
+// Now we create a new build directory and write our output from above in json form in it
+fs.ensureDirSync(buildPath);
+for(let contract in output) {
+  fs.outputJsonSync(
+    path.resolve(buildPath, contract + '.json'),
+    output[contract]
+  );
+}
